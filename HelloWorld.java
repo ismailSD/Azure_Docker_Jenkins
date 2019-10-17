0176f7e1cefa0ce640c4ca39336c1e2430a59dcd
Ubuntu <iadam200@lab1VM.n3mhe4fopshezgsukwuo4svuuh.bx.internal.cloudapp.net>
2019-10-17 20:01:02 +0000

    add welcome to jenkins print line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,11 @@
 /* This is a simple Java program. 
    FileName : "HelloWorld.java". */
-class HelloWorld 
-{ 
+class HelloWorld { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
+    public static void main(String args[]) { 
+	System.out.println("Hello, World");
+        System.out.println("Welcome to jenkins"); 
         System.out.println("Test Line 1"); 
         System.out.println("Test Line 2"); 
         System.out.println("Test Line 3"); 
